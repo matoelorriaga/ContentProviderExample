83f329a7029373502ca97bd4a7b420119258f12f
Matías Elorriaga <matoelorriaga@gmail.com>
2015-01-03 19:46:59 -0300

    added 'contentprovider' package

---
+++ app/src/main/java/com/melorriaga/contentproviderexample/contentprovider/TodoContentProvider.java
@@ -0,0 +1,199 @@
+package com.melorriaga.contentproviderexample.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.melorriaga.contentproviderexample.database.TodoDatabaseHelper;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class TodoContentProvider extends ContentProvider {
+
+    // database
+    private TodoDatabaseHelper database;
+
+    // used by the UriMatcher
+    private static final int TODOS = 1;
+    private static final int TODO_ID = 2;
+
+    private static final String AUTHORITY = "com.melorriaga.contentproviderexample.contentprovider";
+    private static final String BASE_PATH = "todos";
+
+    public static final Uri CONTENT_URI = Uri.parse(String.format(
+            "content://%s/%s",
+            AUTHORITY,
+            BASE_PATH));
+
+    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/todos";
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/todo";
+
+    private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        URI_MATCHER.addURI(AUTHORITY, BASE_PATH, TODOS);
+        URI_MATCHER.addURI(AUTHORITY, BASE_PATH + "/#", TODO_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        database = new TodoDatabaseHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        // check if the caller has requested a column which does not exists
+        checkColumns(projection);
+
+        // use query builder
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        // set the table
+        queryBuilder.setTables(TodoDatabaseHelper.TodoTable.TABLE_TODO);
+
+        int uriType = URI_MATCHER.match(uri);
+        switch (uriType) {
+            case TODOS:
+                break;
+            case TODO_ID:
+                // add the 'where' condition
+                queryBuilder.appendWhere(TodoDatabaseHelper.TodoTable.COLUMN_ID + " = "
+                        + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri.toString());
+        }
+
+        // perform the query
+        Cursor cursor = queryBuilder.query(database.getReadableDatabase(), projection, selection,
+                selectionArgs, null, null, sortOrder);
+
+        // make sure that potential listeners are getting notified
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+
+        long id = 0;
+
+        int uriType = URI_MATCHER.match(uri);
+        switch (uriType) {
+            case TODOS:
+                id = database.getWritableDatabase().insert(
+                        TodoDatabaseHelper.TodoTable.TABLE_TODO, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri.toString());
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // return the uri for the newly inserted item
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+
+        int rowsDeleted = 0;
+
+        int uriType = URI_MATCHER.match(uri);
+        switch (uriType) {
+            case TODOS:
+                rowsDeleted = database.getWritableDatabase().delete(
+                        TodoDatabaseHelper.TodoTable.TABLE_TODO, selection, selectionArgs);
+                break;
+            case TODO_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsDeleted = database.getWritableDatabase().delete(
+                            TodoDatabaseHelper.TodoTable.TABLE_TODO,
+                            TodoDatabaseHelper.TodoTable.COLUMN_ID + " = " + id,
+                            null);
+                } else {
+                    rowsDeleted = database.getWritableDatabase().delete(
+                            TodoDatabaseHelper.TodoTable.TABLE_TODO,
+                            TodoDatabaseHelper.TodoTable.COLUMN_ID + " = " + id
+                                    + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri.toString());
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int rowsUpdated = 0;
+
+        int uriType = URI_MATCHER.match(uri);
+        switch (uriType) {
+            case TODOS:
+                rowsUpdated = database.getWritableDatabase().update(
+                        TodoDatabaseHelper.TodoTable.TABLE_TODO, values, selection, selectionArgs);
+                break;
+            case TODO_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsUpdated = database.getWritableDatabase().update(
+                            TodoDatabaseHelper.TodoTable.TABLE_TODO,
+                            values,
+                            TodoDatabaseHelper.TodoTable.COLUMN_ID + " = " + id,
+                            null);
+                } else {
+                    rowsUpdated = database.getWritableDatabase().update(
+                            TodoDatabaseHelper.TodoTable.TABLE_TODO,
+                            values,
+                            TodoDatabaseHelper.TodoTable.COLUMN_ID + " = " + id
+                                    + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri.toString());
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return rowsUpdated;
+    }
+
+    private void checkColumns(String[] projection) {
+        String[] available = {
+                TodoDatabaseHelper.TodoTable.COLUMN_ID,
+                TodoDatabaseHelper.TodoTable.COLUMN_CATEGORY,
+                TodoDatabaseHelper.TodoTable.COLUMN_SUMMARY,
+                TodoDatabaseHelper.TodoTable.COLUMN_DESCRIPTION
+        };
+        if (projection != null) {
+            HashSet<String> requestedColumns = new HashSet<>(Arrays.asList(projection));
+            HashSet<String> availableColumns = new HashSet<>(Arrays.asList(available));
+            // check if all columns which are requested are available
+            if (!availableColumns.containsAll(requestedColumns)) {
+                throw new IllegalArgumentException("Unknown columns in projection");
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/melorriaga/contentproviderexample/database/TodoDatabaseHelper.java
+++ app/src/main/java/com/melorriaga/contentproviderexample/database/TodoDatabaseHelper.java
@@ -6,9 +6,44 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class TodoDatabaseHelper extends SQLiteOpenHelper {
 
+    public static class TodoTable {
+
+        // database table
+        public static final String TABLE_TODO = "todo";
+
+        // database columns
+        public static final String COLUMN_ID = "_id";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_SUMMARY = "summary";
+        public static final String COLUMN_DESCRIPTION = "description";
+
+        // database create statement
+        private static final String DATABASE_CREATE = String.format(
+                "CREATE TABLE %s (" +
+                        "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "%s TEXT NOT NULL, " +
+                        "%s TEXT NOT NULL, " +
+                        "%s TEXT NOT NULL" +
+                        ");",
+                TABLE_TODO,
+                COLUMN_ID,
+                COLUMN_CATEGORY,
+                COLUMN_SUMMARY,
+                COLUMN_DESCRIPTION);
+
+        // database drop statement
+        private static final String DATABASE_DROP = String.format(
+                "DROP TABLE IF EXISTS %s",
+                TABLE_TODO);
+
+    }
+
+    // database name
     private static final String DATABASE_NAME = "todotable.db";
+    // database version
     private static final int DATABASE_VERSION = 1;
 
+    // constructor
     public TodoDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -16,14 +51,15 @@ public class TodoDatabaseHelper extends SQLiteOpenHelper {
     // this method is called during the creation of the database
     @Override
     public void onCreate(SQLiteDatabase database) {
-        TodoTable.onCreate(database);
+        database.execSQL(TodoTable.DATABASE_CREATE);
     }
 
     // this method is called during an upgrade if the database,
     // e.g. if you increase the database version
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-        TodoTable.onUpgrade(database, oldVersion, newVersion);
+        database.execSQL(TodoTable.DATABASE_DROP);
+        database.execSQL(TodoTable.DATABASE_CREATE);
     }
 
 }

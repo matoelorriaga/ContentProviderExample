855b0445c263b79a782dbd41d4385447e59f3791
Matías Elorriaga <matoelorriaga@gmail.com>
2015-01-03 00:01:50 -0300

    removed unnecessary files

--- app/src/main/java/com/melorriaga/contentproviderexample/TodosListActivity.java
+++ app/src/main/java/com/melorriaga/contentproviderexample/TodosListActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class TodosListActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.todos_list_activity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.todos_list_menu, menu);
         return true;
     }
 
